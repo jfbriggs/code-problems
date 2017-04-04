d0ed913fa8805e87100cd5262d9b2899f9b29627
James Briggs <jfbriggs106@gmail.com>
2017-04-04 15:32:32 -0700

    Modify smallestCommonNumber for sorted arrays

--- smallestCommonNumber.js
+++ smallestCommonNumber.js
@@ -3,28 +3,69 @@
 
 var smallestCommon = function(a1, a2, a3) {
 
-  var first = a1;
-  var second = objectConvert(a2);
-  var third = objectConvert(a3);
-
   var lowestCommon = null;
+  
+  // variables to represent current positions in each array
+  var curr1 = 0;
+  var curr2 = 0;
+  var curr3 = 0;
 
-  for (var i = 0; i < first.length; i++) {
-    if (lowestCommon === null || first[i] < lowestCommon) {
-      if (second[first[i]]) {
-        if (third[first[i]]) {
-          lowestCommon = first[i];
-        }
+  var matchValue = a1[0];
+
+  // inner function to increment arrays (takes in a value to try and match)
+  var findMatch = function() {
+    // while current value in array 1 is less than value to match
+    while (a1[curr1] < matchValue) {
+      // increment curr1
+      curr1++;
+      // if the new current value doesn't exist (i.e. index greater than last index in array, return -1)
+      if (curr1 >= a1.length) {
+        return -1;
+      }
+      // if the current value in array1 is now greater than matchValue, set matchValue to it
+      if (a1[curr1] > matchValue) {
+        matchValue = a1[curr1];
       }
     }
+
+    while (a2[curr2] < matchValue) {
+      // increment curr1
+      curr2++;
+      // if the new current value doesn't exist (i.e. index greater than last index in array, return -1)
+      if (curr2 >= a2.length) {
+        return -1;
+      }
+      // if the current value in array1 is now greater than matchValue, set matchValue to it
+      if (a2[curr2] > matchValue) {
+        matchValue = a2[curr2];
+      }
+    }
+
+    while (a3[curr3] < matchValue) {
+      // increment curr1
+      curr3++;
+      // if the new current value doesn't exist (i.e. index greater than last index in array, return -1)
+      if (curr3 >= a3.length) {
+        return -1;
+      }
+      // if the current value in array1 is now greater than matchValue, set matchValue to it
+      if (a3[curr3] > matchValue) {
+        matchValue = a3[curr3];
+      }
+    }
+
+    // if the three current values are the same, return that value
+    if (a1[curr1] === a2[curr2] && a2[curr2] === a3[curr3]) {
+      return a1[curr1];
+    } else {
+      // otherwise, reinvoke inner function
+      return findMatch();
+    }
+    
   }
 
-  return lowestCommon;
+  // invoke inner function first time with a return
+  return findMatch();
 }
 
-var objectConvert = function(array) {
-  return array.reduce(function(obj, element) {
-    obj[element] = element;
-    return obj;
-  }, {});
-}
+console.log(smallestCommon([1, 2, 3, 4, 7, 8, 9], [0, 2, 4, 9], [1, 3, 4, 9, 10, 15]));
