280d7daedea474b3fdcffdd599e52ab27a68b840
James Briggs <jfbriggs106@gmail.com>
2017-04-04 15:38:23 -0700

    Modify smallestCommonNumber to work with 1 coded loop

--- smallestCommonNumber.js
+++ smallestCommonNumber.js
@@ -10,62 +10,30 @@ var smallestCommon = function(a1, a2, a3) {
   var curr2 = 0;
   var curr3 = 0;
 
-  var matchValue = a1[0];
+  // while iterators are within bounds of the arrays
+  while (curr1 < a1.length && curr2 < a2.length && curr3 < a3.length) {
+    // if the three current values are equal to each other
+    if (a1[curr1] === a2[curr2] && a2[curr2] === a3[curr3]) {
+      // return that value
+      return a1[curr1];
+    }
 
-  // inner function to increment arrays (takes in a value to try and match)
-  var findMatch = function() {
-    // while current value in array 1 is less than value to match
-    while (a1[curr1] < matchValue) {
+    // if the current value in a1 is less than or equal to the current values of a2 and a3
+    if (a1[curr1] <= a2[curr2] && a1[curr1] <= a3[curr3]) {
       // increment curr1
       curr1++;
-      // if the new current value doesn't exist (i.e. index greater than last index in array, return -1)
-      if (curr1 >= a1.length) {
-        return -1;
-      }
-      // if the current value in array1 is now greater than matchValue, set matchValue to it
-      if (a1[curr1] > matchValue) {
-        matchValue = a1[curr1];
-      }
-    }
-
-    while (a2[curr2] < matchValue) {
-      // increment curr1
+    // if the current value in a2 is less than or equal to current values of a1 and a3
+    } else if (a2[curr2] <= a1[curr1] && a2[curr2] <= a3[curr3]) {
+      // increment curr2
       curr2++;
-      // if the new current value doesn't exist (i.e. index greater than last index in array, return -1)
-      if (curr2 >= a2.length) {
-        return -1;
-      }
-      // if the current value in array1 is now greater than matchValue, set matchValue to it
-      if (a2[curr2] > matchValue) {
-        matchValue = a2[curr2];
-      }
-    }
-
-    while (a3[curr3] < matchValue) {
-      // increment curr1
+    // if the current value in a3 is less than or equal to current values in a1 and a2
+    } else if (a3[curr3] <= a1[curr1] && a3[curr3] <= a2[curr2]) {
+      // increment curr3
       curr3++;
-      // if the new current value doesn't exist (i.e. index greater than last index in array, return -1)
-      if (curr3 >= a3.length) {
-        return -1;
-      }
-      // if the current value in array1 is now greater than matchValue, set matchValue to it
-      if (a3[curr3] > matchValue) {
-        matchValue = a3[curr3];
-      }
     }
-
-    // if the three current values are the same, return that value
-    if (a1[curr1] === a2[curr2] && a2[curr2] === a3[curr3]) {
-      return a1[curr1];
-    } else {
-      // otherwise, reinvoke inner function
-      return findMatch();
-    }
-    
   }
 
-  // invoke inner function first time with a return
-  return findMatch();
+  return -1;
 }
 
 console.log(smallestCommon([1, 2, 3, 4, 7, 8, 9], [0, 2, 4, 9], [1, 3, 4, 9, 10, 15]));
