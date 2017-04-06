6e6df5ff11ad92e6d5ef453de5bfbc06a1414054
James Briggs <jfbriggs106@gmail.com>
2017-04-05 18:48:35 -0700

    Clean up largestSumSubarray, remove in-code comments

--- largestSumSubarray.js
+++ largestSumSubarray.js
@@ -12,34 +12,24 @@ Logic:
 */
 
 var largestSumSubarray = function(array) {
-  // create currentMax & globalMax vars, set to first array element
   var currentMaxSum = array[0], globalMaxSum = array[0];
   var currentMaxIndices = [0, 0], globalMaxIndices = [0, 0];
-  // iterate through array starting at second element
+
   for (var i = 1; i < array.length; i++) {
-    // if currentMax value is negative
     if (currentMaxSum < 0) {
-      // set currentMax to the current element in the loop
       currentMaxSum = array[i];
       currentMaxIndices = [i, i];
-    } else { // otherwise
-      // add the current element in the loop to currentMax
+    } else {
       currentMaxSum += array[i];
       currentMaxIndices[1] = i;
     }
 
-    // if globalMax is less than currentMax, set globalMax to what currentMax is
     if (globalMaxSum < currentMaxSum) {
       globalMaxSum = currentMaxSum;
       globalMaxIndices[0] = currentMaxIndices[0];
       globalMaxIndices[1] = currentMaxIndices[1];
     }
-
-    
   }
 
-  // output globalMax
   return globalMaxIndices;
 };
-
-console.log(largestSumSubarray([-4, 2, -5, 1, 2, 3, 6, -5, 1]));
