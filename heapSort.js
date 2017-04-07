c27d077ddec1584f94d77e452345fbc0b8e1a80d
James Briggs <jfbriggs106@gmail.com>
2017-04-06 17:39:31 -0700

    Clean up heapSort code, remove in-code comments

--- heapSort.js
+++ heapSort.js
@@ -14,39 +14,29 @@ Heapsort: Runs build max heap once, then iterates downward from last index; swap
 */
 
 var heapify = function(array, i, n) {
-  // variable to represent left child
-  // variable to represent right child
   var left = 2 * i + 1, right = 2 * i + 2;
-  // variable to represent max value in 3-way comparison
   var max;
-  // if left child is in bounds and is greater than current element
+
   if (left <= n && array[left] > array[i]) {
-    // max becomes left child
     max = left;
-  } else { // otherwise
-    // max becomes current element
+  } else {
     max = i;
   }
-  // if right child is in bounds and is greater than max
+
   if (right <= n && array[right] > array[max]) {
-    // max becomes right child
     max = right;
   }
-  // if max is not the current element
+
   if (max !== i) {
-    // swap the current element with max
     var temp = array[i];
     array[i] = array[max];
     array[max] = temp;
-    // re-invoke heapify on new index
     heapify(array, max);
   }
 };
 
 var buildMaxHeap = function(array) {
-  // iterate downwards from rounded down midpoint
   for (var i = Math.floor(array.length / 2); i >= 0; i--) {
-    // for each element, run heapify on it (pass in array length - 1 as n)
     heapify(array, i, array.length - 1);
   }
 
@@ -54,25 +44,16 @@ var buildMaxHeap = function(array) {
 };
 
 var heapSort = function(array) {
-  // invoke build max heap to create a max heap off the bat
   array = buildMaxHeap(array);
-  console.log('Initial max heap', array);
-  // set a variable to represent current rightmost point of active heap (the chunk of the array not officially sorted) (starts at array length - 1)
   var n = array.length - 1;
-  // while the above variable is greater than 0
+
   while (n > 0) {
-    // swap the first value in the array with the value at the rightmost point
     var temp = array[0];
     array[0] = array[n];
     array[n] = temp;
-    // decrement the rightmost point variable
     n--;
-    // invoke heapify on the first value in the array
     heapify(array, 0, n);
   }
-  // output the array
+
   return array;
 };
-
-var arr = [10, 1, 6, 2, 25, 15, 9, 8, 3, 11, 7, 40, 4];
-console.log(heapSort(arr));
