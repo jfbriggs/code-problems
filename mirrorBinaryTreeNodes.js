76add8bf43baa0ba8efd0cdc1982b898deead4e8
James Briggs <jfbriggs106@gmail.com>
2017-04-09 19:44:37 -0700

    Clean up mirrorBinaryTreeNodes, remove in-code comments

--- mirrorBinaryTreeNodes.js
+++ mirrorBinaryTreeNodes.js
@@ -12,65 +12,25 @@ Logic:
 */
 
 var mirrorNodes = function(root) {
-  // inner function to swap a node's children
   var swapChildren = function(node) {
-    // BASE CASE: if node's left is null AND node's right is null
     if (!node.left && !node.right) {
-      // break out;
       return;
     }
 
-    // temp variable to represent left (or null)
     var temp = node.left;
-    // set left equal to what right is (or null)
     node.left = node.right;
-    // set right equal to what temp is
     node.right = temp;
-    // re-invoke this function on left
+
     if (node.left) {
       swapChildren(node.left);
     }
 
-    // re-invoke this function on right
     if (node.right) {
       swapChildren(node.right);
     }
   }
 
-
-  // invoke function first time
   swapChildren(root);
 
   return root;
 }
-
-var Tree = function(val) {
-  this.value = val;
-  this.left = null;
-  this.right = null;
-}
-
-Tree.prototype.addLeft = function(node) {
-  this.left = node;
-}
-
-Tree.prototype.addRight = function(node) {
-  this.right = node;
-}
-
-var a = new Tree(1);
-var b = new Tree(2);
-var c = new Tree(3);
-var d = new Tree(4);
-var e = new Tree(5);
-var f = new Tree(6);
-var g = new Tree(7);
-var h = new Tree(8);
-
-a.addLeft(b);
-a.addRight(c);
-b.addLeft(d);
-b.addRight(e);
-c.addLeft(f);
-c.addRight(g);
-d.addRight(h);
