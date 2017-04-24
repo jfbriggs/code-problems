153cd62eb8d42595c7bec95058353e4558bde576
James Briggs <jfbriggs106@gmail.com>
2017-04-24 15:51:34 -0700

    Clean up updated quickSort code, remove comments

--- quicksort.js
+++ quicksort.js
@@ -15,67 +15,44 @@ LOGIC:
 
 */
 
-var quicksort = function(array) {
+var quickSort = function(array) {
 
-  var orgByPivot = function(left, right) {
-    console.log(left, right);
-    // BASE CASE: if left and right are the same value
+  var partialSort = function(left, right) {
     if (left >= right) {
-      // break out
       return;
     }
 
-    // set pivot to what right is
     var pivot = right;
-    // set right to right - 1
     right = right - 1;
-    // set originalRight to what pivot is
-    // set originalLeft to what left is
     var origRight = pivot, origLeft = left;
 
-    // while left and right are not the same value
     while (left !== right) {
-      
-      // if value at left is less than or equal to value at pivot
       if (array[left] <= array[pivot]) {
-        // increment left
         left++;
-      } else {  // otherwise (if value at left is greater than value at pivot)
-        // if value at right is greater than value at pivot
+      } else {
         if (array[right] > array[pivot]) {
-          // decrement right
           right--;
-        } else { // otherwise
-          // swap values at left and right
+        } else {
           var temp = array[left];
           array[left] = array[right];
           array[right] = temp;          
         }
       }
-
     }
 
-    console.log(array);
-    console.log(left, right);
-
-    // if value at convergence point is greater than pivot element
     if (array[left] > array[pivot]) {
-      // swap them
       var temp = array[left];
       array[left] = array[pivot];
       array[pivot] = temp;      
-      // re-invoke inner function on subarray to left of convergence index
-      orgByPivot(origLeft, left - 1);
-      // re-invoke inner function on subarray to right of convergence index
-      orgByPivot(left + 1, origRight);
+      partialSort(origLeft, left - 1);
+      partialSort(left + 1, origRight);
     } else {
-      orgByPivot(origLeft, pivot - 1);
+      partialSort(origLeft, pivot - 1);
     }
-
 
   }
 
-  orgByPivot(0, array.length - 1);
+  partialSort(0, array.length - 1);
   
   return array;
 }
