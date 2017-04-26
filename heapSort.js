9e95223721168247729844769c5ce9562267bea7
James Briggs <jfbriggs106@gmail.com>
2017-04-26 13:07:12 -0700

    Re-write heapSort

--- heapSort.js
+++ heapSort.js
@@ -13,47 +13,62 @@ Heapsort: Runs build max heap once, then iterates downward from last index; swap
 
 */
 
-var heapify = function(array, i, n) {
-  var left = 2 * i + 1, right = 2 * i + 2;
-  var max;
-
-  if (left <= n && array[left] > array[i]) {
-    max = left;
-  } else {
-    max = i;
+var heapify = function(array, i, last) {
+  // max variable and set it to parent index
+  var max = i;
+  // variables to represent child indices
+  var first = i * 2 + 1, second = i * 2 + 2;
+  // if first child exists and is greater than parent but less than or equal to last
+  if (first <= last && array[first] > array[max]) {
+    // set that child index to be new max
+    max = first;
   }
 
-  if (right <= n && array[right] > array[max]) {
-    max = right;
+  // if second child exists and is greater than parent but less than or equal to last
+  if (second <= last && array[second] > array[max]) {
+    // set that child index to be new max
+    max = second;
   }
 
+  // if element at max index is not the same value as element at original index
   if (max !== i) {
+    // swap them
     var temp = array[i];
     array[i] = array[max];
     array[max] = temp;
-    heapify(array, max, n);
+    // re-run heapify on new max index
+    heapify(array, max, last);
+  } else { // otherwise (if original element is still greatest)
+    // output the array
+    return array;
   }
-};
+
+}
 
 var buildMaxHeap = function(array) {
+  // iterate backward from array length / 2 rounded down
   for (var i = Math.floor(array.length / 2); i >= 0; i--) {
+    // run heapify on current index
     heapify(array, i, array.length - 1);
   }
 
+  // return array
   return array;
-};
+}
 
 var heapSort = function(array) {
+  // invoke buildMaxHeap on the array
   array = buildMaxHeap(array);
-  var n = array.length - 1;
-
-  while (n > 0) {
+  // iterate backward from last element in the array (stopping at first index)
+  for (var i = array.length - 1; i > 0; i--) {
+    // swap the current element with the first element
     var temp = array[0];
-    array[0] = array[n];
-    array[n] = temp;
-    n--;
-    heapify(array, 0, n);
+    array[0] = array[i];
+    array[i] = temp;
+    // run heapify, passing in first index and current - 1
+    heapify(array, 0, i - 1);
   }
 
+  // output array
   return array;
-};
+}
