0562f31d7d9288d931f51f56991738da4583b160
James Briggs <jfbriggs106@gmail.com>
2017-04-26 11:49:43 -0700

    Clean up bubbleSort code, remove comments

--- bubbleSort.js
+++ bubbleSort.js
@@ -14,40 +14,29 @@ ENDING CONDITIONS:
 */
 
 var bubbleSort = function(array) {
-  // variable to represent whether any swaps happened in the prior iteration (true by default)
   var swapped = true;
 
   var iterate = function(lastIndex) {
-    // BASE CASE: if swapped variable is false, or last Index is 0
     if (!swapped || lastIndex === 0) {
-      // break out
       return;
     }
 
-    // set swapped variable to false
     swapped = false;
-    // iterate forward through array
+
     for (var i = 0; i < lastIndex; i++) {
-      // if current element in iteration is greater than the next element
       if (array[i] > array[i + 1]) {
-        // swap them
         var temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
-        // set swapped variable to true
         swapped = true;
       }
 
     }
 
-    // re-invoke iterate function with lastIndex being 1 less than it currently is
     iterate(lastIndex - 1);
   }
 
-  // invoke iterate for first time
   iterate(array.length - 1);
 
-  // output the array
   return array;
-
 }
