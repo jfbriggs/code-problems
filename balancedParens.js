4c2faca0ffd4cdea593e3696b79afee46615de1c
James Briggs <jfbriggs106@gmail.com>
2017-05-04 13:25:52 -0700

    Clean up balancedParens, remove in-code comments

--- balancedParens.js
+++ balancedParens.js
@@ -34,36 +34,20 @@
 
 
 var balancedParens = function(input) {
-  // object to represent just opening bracket types
   var openers = { "(": true, "{": true, "[": true };
-  // object to represent equivalent openers for closing bracket types
   var openerEquivalents = { ")": "(", "}": "{", "]": "[" };
-  // array to represent active opening brackets
   var activeOpens = [];
-  // iterate through input string
+
   for (var i = 0; i < input.length; i++) {
-    // if the current character is some type of opening bracket
     if (openers[input[i]]) {
-      // add it to the active opening brackets array
       activeOpens.push(input[i]);
-    } else if (openerEquivalents[input[i]]) { // Or, if the current character exists as a key in the opener/closer object
-      // if the associated value is the same as the last element in the current active opening brackets array
+    } else if (openerEquivalents[input[i]]) {
       if (activeOpens.length === 0 || openerEquivalents[input[i]] !== activeOpens[activeOpens.length - 1]) {
-        // immediately output false
         return false;    
       }
-      // otherwise, remove the last element in the current active opening brackets array
       activeOpens.pop();
     }    
   }
 
-  // if the active opening brackets array still has something in it, output false.  otherwise, true.
   return activeOpens.length > 0 ? false : true;
-
 };
-
-console.log(balancedParens('[](){}'));  // true
-console.log(balancedParens('[({})]')); // true
-console.log(balancedParens('[(]{)}'));  // false
-console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
-console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
