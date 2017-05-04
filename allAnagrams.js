9b9ea50d11337d987fb5b522fa900ca5e9aae361
James Briggs <jfbriggs106@gmail.com>
2017-05-04 12:47:56 -0700

    Clean up allAnagrams, remove in-code comments

--- allAnagrams.js
+++ allAnagrams.js
@@ -13,29 +13,19 @@
   */
 
 var allAnagrams = function(string) {
-  // create results array
   var results = [];
 
-  // create inner function to select a character and add to a combo (accept a string, and an existing combo string)
   var createCombo = function(choices, currentCombo) {
-    // BASE CASE:  if combo string is same length as original string
     if (currentCombo.length === string.length) {
-      // add the combo string to results array
       results.push(currentCombo);
     }
 
-    // iterate through choices string
     for (var i = 0; i < choices.length; i++) {
-      // invoke createCombo, passing in combo with current letter cut out, and existing combo + current letter
       createCombo(choices.slice(0, i) + choices.slice(i + 1), currentCombo + choices[i]);
     }
   }
 
-  // invoke inner function for the first time
   createCombo(string, '');
-  // output the results array
   return results;
 
 };
-
-console.log(allAnagrams('abc'));
