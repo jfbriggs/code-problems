969e4a4f9819ded7c5089d31fcb4a5b18c839d23
James Briggs <jfbriggs106@gmail.com>
2017-05-08 14:00:05 -0700

    Clean up characterFrequency, remove in-code comments

--- characterFrequency.js
+++ characterFrequency.js
@@ -40,60 +40,45 @@ LOGIC:
 -  iterate through the string
 -  create an object that keeps counts of each letter
 
--  iterate through counts object and push each key/value pair to an array
--  iterate through the array, sorting by frequency values
--  iterate through once more, swapping greater letters forward if frequencies are the same
+-  iterate through counts object
+  - each iteration, add current key/value pair as a subarray to front of resulting array
+  - bubblesort style, iterate forward with new pair, swapping as necessary, until it's in place
 
  */
 
 var characterFrequency = function(string) {
-  // object to collect frequency values
   var frequencies = {};
-  // result array
   var frequencyList = [];
-  // iterate through string
+
   for (var i = 0; i < string.length; i++) {
     var current = string[i];
-    // each iteration, if the current character does not exist in frequencies object yet
+
     if (!frequencies[current]) {
-      // create it and set it to 1
       frequencies[current] = 1;
-    } else { // otherwise, if it does already exist
-      // increment it
+    } else {
       frequencies[current]++;      
     }
   }
 
-  // iterate through frequencies object
   for (char in frequencies) {
-    // each iteration, add key/value pair as a subarray to front of result array
     frequencyList.unshift([char, frequencies[char]]);
-    // run sortForward on result as it now is
     frequencyList = sortForward(frequencyList);
   }
 
-  // output result array
   return frequencyList;
 };
 
 var sortForward = function(freqs) {
   if (freqs.length > 1) {
-    // variable to represent the first (new) pair (index value)
     var newIndex = 0;
-    // while the new pair's second element is greater than the next pair's second element OR the new pair and next pair's frequencies are the same but the new pair's letter is greater
+
     while (freqs[newIndex][1] > freqs[newIndex + 1][1] || (freqs[newIndex][1] === freqs[newIndex + 1][1] && freqs[newIndex][0].charCodeAt(0) > freqs[newIndex + 1][0].charCodeAt(0))) {
-      // swap the current pair with its next pair
       var temp = freqs[newIndex];
       freqs[newIndex] = freqs[newIndex + 1];
       freqs[newIndex + 1] = temp;
-      // increment newIndex
       newIndex++;    
     }
   }
 
-  // output freqs
   return freqs;
 }
-
-console.log(characterFrequency('wapslwgiidwpappwkdhguwowga'));
-console.log(sortForward([['g', 7], ['b', 2], ['c', 4], ['f', 7], ['d', 9]]));
