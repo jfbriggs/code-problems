04290ef2a409e5b106ee5c367be3165c3db25f81
James Briggs <jfbriggs106@gmail.com>
2017-05-08 22:56:39 -0700

    Clean up commonCharacters, remove in-code comments

--- commonCharacters.js
+++ commonCharacters.js
@@ -11,40 +11,26 @@
  */
 
 var commonCharacters = function(s1, s2) {
-  // result string variable
   var result = '';
-  // object that will become key value representation of characters in string 2
   var s2Characters = {};
-  // object that will represent characters common between the two strings
   var commonChars = {};
 
-  // iterate through string 2
   for (var i = 0; i < s2.length; i++) {
-    // create a key with value true for that character
     s2Characters[s2[i]] = true;
   }
 
-  // iterate through string 1
   for (var j = 0; j < s1.length; j++) {
-    // if the current character exists as a key in s2Characters
     if (s2Characters[s1[j]]) {
-      // add that character as a key with value true to commonChars
       commonChars[s1[j]] = true;
     }
   }
 
-  // iterate through string 1 one more time
   for (var k = 0; k < s1.length; k++) {
-    // if the current character exists in commonChars with a truthy value
     if (commonChars[s1[k]]) {
-      // add that character to the result string
       result += s1[k];
-      // and set its value in commonChars to false so it wont be recognized again (prevents duplicates)
       commonChars[s1[k]] = false;
     }
   }
 
   return result;
 };
-
-console.log(commonCharacters('acexivou', 'aegihobu')); // 'aeiou'
