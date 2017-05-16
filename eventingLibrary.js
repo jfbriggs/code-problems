847a18e6911e3e0af0aefc277f7b14b5392a03f2
James Briggs <jfbriggs106@gmail.com>
2017-05-16 13:32:31 -0700

    Clean up eventingLibrary, remove in-code comments

--- eventingLibrary.js
+++ eventingLibrary.js
@@ -35,29 +35,16 @@ LOGIC:
 */
 
 var mixEvents = function(obj) {
-  // create a events collection within the object
   obj.events = {};
-  // create the "on" method (accepts an event name and a callback function)
+
   obj.on = function(event, cb) {
-    // create a key in the events collection, set it equal to the callback function
     obj.events.event = cb;
   }
 
-  // create the "trigger" method (accepts an event name)
   obj.trigger = function(event) {
-    // create array representing additional arguments passed in
     var args = Array.prototype.slice.call(arguments, 1);
-    // invoke the function associated with the event name as key in the events collection
     obj.events.event.apply(null, args);
   }
 
-  // output original object
   return obj;
 }
-
-var obj = mixEvents({ name: 'Alice', age: 30 });
-obj.on('ageChange', function(b){ // On takes an event name and a callback function
-   console.log('Age changed', b);
-});
-obj.age++;
-obj.trigger('ageChange', 3); // This should call our callback! Should log 'age changed'.
